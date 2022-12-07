05f136f7fef87c237f502fd130c3cd1a9fd0e014
Johan Sjögren <johan.sjogren@neodev.se>
2022-12-07 16:01:25 +0100

    Started day 4

--- day4a/src/main.rs
+++ day4a/src/main.rs
@@ -1,3 +1,22 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let input_vec: Vec<String> = fs::read_to_string("day4.txt")
+        .expect("failed to load")
+        .lines()
+        .map(|t| String::from(t))
+        .collect();
+
+    for line in input_vec {
+        let splits: Vec<&str> = line.split(",").collect();
+        println!("{:?}", splits);
+
+        for assg in splits {
+            let nums: Vec<u32> = assg
+                .split("-")
+                .try_into().expect("Error")
+                .collect();
+        }
+        break;
+    }
 }
