87877781b657fd8a5c87c49b5e50f15efc20f310
Johan Sjögren <johandsjogren@gmail.com>
2022-12-16 11:16:08 +0100

    Day 5 first part

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,39 +1,85 @@
-use itertools::Itertools;
-use std::fs;
+// use itertools::Itertools;
+use std::{fs, collections::VecDeque};
 
-struct Supplies {
-    storage_map: Vec<Vec<char>>,
-}
-
+#[derive(Debug)]
 struct MoveAction {
-    num: u32,
-    from: u32,
-    to: u32,
+    num: usize,
+    from: usize,
+    to: usize,
 }
 
-// impl Supplies {
-//     fn from_strngs(input: &Vec<String>)-> Supplies {
-//     let test: Vec<Vec<&str>> = input
-//          .iter()
-//          .map(|b| b.as_bytes()
-//          .chunks(4)
-//          .map(std::str::from_utf8)
-//          .collect::<Result<Vec<&str>, _>>()
-//          .unwrap()).collect();
-//     }
-// }
+impl MoveAction {
+    fn from_string(input: &str) -> MoveAction {
+        let instructions = input
+            .split_whitespace()
+            .filter_map(|c| c.parse().ok())
+            .collect::<Vec<usize>>();
+        MoveAction {
+            num: instructions[0],
+            from: instructions[1]-1,
+            to: instructions[2]-1,
+        }
+    }
+}
 
-fn main() {
-    let input_vec: Vec<String> = fs::read_to_string("input.txt")
-        .expect("Failed to load file")
-        .lines()
-        .map(|t| String::from(t))
-        .collect();
+#[derive(Debug)]
+struct Supplies {
+    storage_map: Vec<VecDeque<String>>,
+}
 
+impl Supplies {
+    fn from_strings(input: &Vec<String>) -> Supplies {
+        let input: Vec<Vec<&str>> = input
+            .iter()
+            .map(|b| {
+                b.as_bytes()
+                    .chunks(4)
+                    .map(std::str::from_utf8)
+                    .collect::<Result<Vec<&str>, _>>()
+                    .unwrap()
+            })
+            .collect();
+
+        let mut storage: Vec<VecDeque<String>> = Vec::new();
+        for _ in 0..input[0].len() {
+            storage.push(VecDeque::new())
+        }
+
+        for row in input {
+            for (i, item) in row.iter().enumerate() {
+                let cleaned: String = item.trim().replace("[", "").replace("]", "");
+                let to_push = match cleaned.is_empty() {
+                    true => continue,
+                    false => cleaned,
+                };
+                storage[i].push_back(to_push.clone());
+            }
+        }
+        Supplies {
+            storage_map: storage,
+        }
+    }
+
+    fn perform_move(&mut self, move_action: MoveAction) {
+        for _ in 0..move_action.num {
+            let moved = self.storage_map[move_action.from].pop_front().expect("");
+            self.storage_map[move_action.to].push_front(moved);
+        }
+    }
+
+    fn perform_collective_move(&mut self, move_action: MoveAction) {
+        for _ in 0..move_action.num {
+            let moved = self.storage_map[move_action.from].pop_front().expect("");
+            self.storage_map[move_action.to].push_front(moved);
+        }
+    }
+}
+
+fn split_moves_and_suppplies(input: Vec<String>) -> (Vec<String>, Vec<String>) {
     let mut supplies: Vec<String> = Vec::new();
     let mut moves: Vec<String> = Vec::new();
     let mut read_supplies: bool = true;
-    for line in input_vec {
+    for line in input {
         if line.is_empty() {
             read_supplies = false
         } else if read_supplies {
@@ -43,47 +89,25 @@ fn main() {
         }
     }
     supplies.pop();
+    (supplies, moves)
+}
 
-    // let supplies = Supplies::from_strings(supplies);
-
-    let test: Vec<Vec<&str>> = supplies
-        .iter()
-        .map(|b| {
-            b.as_bytes()
-                .chunks(4)
-                .map(std::str::from_utf8)
-                .collect::<Result<Vec<&str>, _>>()
-                .unwrap()
-        })
+fn main() {
+    let input_vec: Vec<String> = fs::read_to_string("input.txt")
+        .expect("Failed to load file")
+        .lines()
+        .map(|t| String::from(t))
         .collect();
 
-    println!("{:?}", test.len());
-    println!("{:?}", test[0].len());
+    let (supplies, moves) = split_moves_and_suppplies(input_vec);
 
-    let mut bla: Vec<Vec<String>> = Vec::new();
-    for x in test[0].iter() {
-        bla.push(Vec::new())
+    let mut supplies = Supplies::from_strings(&supplies);
+    // println!("{:?}", supplies.storage_map);
+    println!("{:?}", MoveAction::from_string(&moves[0]));
+    for move_action in moves {
+        supplies.perform_move(MoveAction::from_string(&move_action));
     }
-
-    println!("{:?}", bla);
-
-    for row in test {
-        for (i, item) in row.iter().enumerate() {
-            let cleaned: String = item.trim().replace("[", "").replace("]", "");
-            let to_push = match cleaned.is_empty() {
-                true => continue,
-                false => cleaned
-                // false => println!("Something {} count {}", cleaned, i),
-            };
-            // println!("{} {}", i, to_push);
-            bla[i].push(to_push.clone());
-        }
-    }
-    
-    println!("{:?}", bla);
-    // for line in &supplies {
-    //     let test: Vec<String> = line.chars().map(|t| String::from(t)).chunks(4).collect();
-    //     println!("{:?}", test);
-    // }
-    // println!("{:?}", moves);
+    println!("{:?}", supplies.storage_map);
+    let firsts: String = supplies.storage_map.iter().map(|vec| String::from(&vec[0])).collect::<String>();
+    println!("{}", firsts);
 }
